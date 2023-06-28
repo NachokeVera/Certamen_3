b767c1e5ab132a0c0f3da14f63404ebe10f18ee6
NachokeVera <ignacio.vera.hidalgo@gmail.com>
2023-06-28 19:10:54 -0400

    Login avanzado

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Request\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginController extends Controller
 {
@@ -10,6 +12,16 @@ class LoginController extends Controller
         return view('artista.login');
     }
     public function iniciar(LoginRequest $request){
+        $credentials = $request->getCredentials()
+        if(!Auth::validate($credentials)){
+            return redirect()->to('artista.login')->withErrors('auth.failed');
+        }
+        $user = Auth::getProvider()->retriveByCredentials($credentials);
+        Auth::login($user);
 
+        return $this->authenticated($request,$user)
+    }
+    public function authenticated(){
+        
     }
 }
